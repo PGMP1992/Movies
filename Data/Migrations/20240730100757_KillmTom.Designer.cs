2240e01e04d85f8b61da42ec94047b80031dc517
PGMP1992 <pedro@dkltd.net>
2024-07-30 13:57:53 +0200

    Created DBSet PlaylistMovies  and Repos/Interface and Killed Many to Many relashionship

--- Data/Migrations/20240730100757_KillmTom.Designer.cs
+++ Data/Migrations/20240730100757_KillmTom.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MoviesApp.Data;
 
@@ -11,9 +12,11 @@ using MoviesApp.Data;
 namespace Movies.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240730100757_KillmTom")]
+    partial class KillmTom
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -285,6 +288,9 @@ namespace Movies.Data.Migrations
                         .HasMaxLength(100)
                         .HasColumnType("nvarchar(100)");
 
+                    b.Property<int>("MovieId")
+                        .HasColumnType("int");
+
                     b.Property<string>("Name")
                         .IsRequired()
                         .HasMaxLength(50)
@@ -294,30 +300,9 @@ namespace Movies.Data.Migrations
 
                     b.HasIndex("AppUserId");
 
-                    b.ToTable("Playlists");
-                });
-
-            modelBuilder.Entity("MoviesApp.Models.PlaylistMovie", b =>
-                {
-                    b.Property<int>("Id")
-                        .ValueGeneratedOnAdd()
-                        .HasColumnType("int");
-
-                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("Id"));
-
-                    b.Property<int>("MovieId")
-                        .HasColumnType("int");
-
-                    b.Property<int>("PlaylistId")
-                        .HasColumnType("int");
-
-                    b.HasKey("Id");
-
                     b.HasIndex("MovieId");
 
-                    b.HasIndex("PlaylistId");
-
-                    b.ToTable("PlaylistMovies");
+                    b.ToTable("Playlists");
                 });
 
             modelBuilder.Entity("Microsoft.AspNetCore.Identity.IdentityRoleClaim<string>", b =>
@@ -379,26 +364,15 @@ namespace Movies.Data.Migrations
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.Navigation("AppUser");
-                });
-
-            modelBuilder.Entity("MoviesApp.Models.PlaylistMovie", b =>
-                {
                     b.HasOne("MoviesApp.Models.Movie", "Movie")
                         .WithMany()
                         .HasForeignKey("MovieId")
                         .OnDelete(DeleteBehavior.Cascade)
                         .IsRequired();
 
-                    b.HasOne("MoviesApp.Models.Playlist", "Playlist")
-                        .WithMany()
-                        .HasForeignKey("PlaylistId")
-                        .OnDelete(DeleteBehavior.Cascade)
-                        .IsRequired();
+                    b.Navigation("AppUser");
 
                     b.Navigation("Movie");
-
-                    b.Navigation("Playlist");
                 });
 
             modelBuilder.Entity("MoviesApp.Models.AppUser", b =>
---
+++ Data/Migrations/20240730100757_KillmTom.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Movies.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class KillmTom : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "MoviePlaylist");
+
+            migrationBuilder.AddColumn<int>(
+                name: "MovieId",
+                table: "Playlists",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Playlists_MovieId",
+                table: "Playlists",
+                column: "MovieId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Playlists_Movies_MovieId",
+                table: "Playlists",
+                column: "MovieId",
+                principalTable: "Movies",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Playlists_Movies_MovieId",
+                table: "Playlists");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Playlists_MovieId",
+                table: "Playlists");
+
+            migrationBuilder.DropColumn(
+                name: "MovieId",
+                table: "Playlists");
+
+            migrationBuilder.CreateTable(
+                name: "MoviePlaylist",
+                columns: table => new
+                {
+                    MovieListId = table.Column<int>(type: "int", nullable: false),
+                    PlaylistListId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MoviePlaylist", x => new { x.MovieListId, x.PlaylistListId });
+                    table.ForeignKey(
+                        name: "FK_MoviePlaylist_Movies_MovieListId",
+                        column: x => x.MovieListId,
+                        principalTable: "Movies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_MoviePlaylist_Playlists_PlaylistListId",
+                        column: x => x.PlaylistListId,
+                        principalTable: "Playlists",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MoviePlaylist_PlaylistListId",
+                table: "MoviePlaylist",
+                column: "PlaylistListId");
+        }
+    }
+}
